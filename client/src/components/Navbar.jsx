cc40b728b923cc6426b78cde60a1ed5d0076f2b7
Shaarvy <shaarvyc@gmail.com>
2025-06-23 20:29:35 +0530

    Initial Commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import {HiMenuAlt4} from 'react-icons/hi';
+import {AiOutlineClone} from 'react-icons/ai';
+import logo from "../../images/logo.png";
+
+const Navbar = () => {
+    return (
+        // <nav className="w-full flex md:justify-center justify-between items-center p-4">
+        //     <div className="md:flex-[0.5] flex-initial justify-center items-center">
+        //         <img src = {logo} alt="logo" className="w-32 cursor-ponter"/>
+        //     </div>
+        // </nav>
+        <h1>Navbar</h1>
+    );
+}
+
+export default Navbar;
