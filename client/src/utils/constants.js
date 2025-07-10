8ee9fbed4399e3d273d87cca2d98f6721a70571e
Shaarvy <shaarvyc@gmail.com>
2025-07-10 19:30:10 +0530

    Created the smart contract

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json0';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x601180972e45Cd700c87Bf1277c4d1Bc0c679F9E';
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,15 @@
+
+//https://eth-sepolia.g.alchemy.com/v2/9HPLg43DaD7r36T_-Tx5v
+require('@nomiclabs/hardhat-waffle');
+require("@nomicfoundation/hardhat-ignition");
+
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    sepolia: {
+      url: "", //Add your url of the network here
+      accounts: [] //Add your private key here as a string
+    }
+  }
+}
