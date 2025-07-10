a526189c55917724a324c8d5f1f2871fcad11a5e
Shaarvy <shaarvyc@gmail.com>
2025-07-10 19:33:20 +0530

    Created the smart contract

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -8,8 +8,8 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     sepolia: {
-      url: "", //Add your url of the network here
-      accounts: [] //Add your private key here as a string
+      url: 'https://eth-sepolia.g.alchemy.com/v2/9HPLg43DaD7r36T_-Tx5v',
+      accounts: ['b834bf13943be6ef6eb4886b6e27b393e0b28d3a5e07414cc3104a8a5a2d33f5']
     }
   }
 }
