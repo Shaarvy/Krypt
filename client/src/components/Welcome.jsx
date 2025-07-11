295e03ea9a53492af13d3e5c6b69a16f09ca838b
Shaarvy <shaarvyc@gmail.com>
2025-07-11 21:53:51 +0530

    Connect Wallet

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,7 +1,9 @@
+import React, { useContext } from 'react';
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from 'react-icons/si';
 import { BsInfoCircle } from 'react-icons/bs';
 import { Loader } from "./";
+import { TransactionContext } from "../context/TransactionContext";
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-gray-400";
 
@@ -17,10 +19,8 @@ const Input = ({placeholder, name, type, value, handleChange}) => (
 );
 
 const Welcome = () => {
-    const connetWallet = () => {
-        
-    }
-
+    const { connectWallet, currentAccount } = useContext(TransactionContext);
+  
     const handleSubmit = () => {
 
     }
@@ -35,14 +35,16 @@ const Welcome = () => {
                     <p className="text-left mt-5 text-white font-white md:w-9/12 w-11/12 text-base">
                         Explore the crypto world. Buy and sell cryptocurrencies easily on Krypt.
                     </p>
-                    <button
-                        type="button"
-                        onClick={connetWallet}
-                        className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-4 cursor-pointer hover:bg-[#2546bd]"
-                    >
-                        <p className="text-white text-base font-semibold">Connect Wallet</p>
+                    {!currentAccount && (
+                        <button
+                            type="button"
+                            onClick={connectWallet}
+                            className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-4 cursor-pointer hover:bg-[#2546bd]"
+                        >
+                            <p className="text-white text-base font-semibold">Connect Wallet</p>
 
-                    </button>
+                        </button>
+                    )}
                     <div className="grid sm:grid-cols-3 grip-cols-2 w-full mt-10">
                         <div className={`rounded-tl-2xl ${commonStyles}`}>
                             Reliability
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -8,8 +8,8 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     sepolia: {
-      url: 'https://eth-sepolia.g.alchemy.com/v2/9HPLg43DaD7r36T_-Tx5v',
-      accounts: ['b834bf13943be6ef6eb4886b6e27b393e0b28d3a5e07414cc3104a8a5a2d33f5']
+      url: '', //Add your url
+      accounts: []; //Add your private key
     }
   }
 }
