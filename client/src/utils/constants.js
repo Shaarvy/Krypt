fd5cd7df56a2d2183c54f9de69fcb5fb8baae43b
Shaarvy <shaarvyc@gmail.com>
2025-07-19 23:21:30 +0530

    Updated

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from './Transactions';
 
 export const contractABI = abi.abi;
-export const contractAddress = '0x04EE2087fE84e6c4F6Aaf9738717052754Ac6C3e';
+export const contractAddress = //ADD YOUR CONTRACT ADDRESS;
